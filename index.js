b00f8b0ab37b830c32044a9c36fd1b92a420c5d0
James <36522308+renniak@users.noreply.github.com>
2021-05-01 16:38:52 -0300

    change PORT and add Procfile

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const PORT = 3001
 
 
 let persons = [
@@ -104,6 +103,7 @@ app.post('/api/persons/', (req, res) => {
     res.json(person)
 })
 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
