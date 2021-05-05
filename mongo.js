b9a58ca315f34fcc97068a44aef17b0463b58af8
James <36522308+renniak@users.noreply.github.com>
2021-05-04 22:30:47 -0300

    finish exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+console.log(password, name, number)
+
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.ro5o3.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (!name || !number) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    
+    person.save().then(result => {
+        console.log('person added')
+        mongoose.connection.close()
+    })  
+}
+
+
+
